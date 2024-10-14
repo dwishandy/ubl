0c2555ee5a888d3a185e9553fb69a062c0efaf84
firshandy <firshandydwi@gmail.com>
2024-10-14 20:35:33 +0700

    third commit

---
+++ contactpage/script.js
@@ -0,0 +1,27 @@
+// toogle navbar
+let navToggle = document.querySelector(".nav_toggle");
+let navWrapper = document.querySelector(".nav_wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
+
+// navbarfixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("nav-fixed");
+  } else {
+    header.classList.remove("nav-fixed");
+  }
+};
