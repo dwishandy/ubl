faad0d02660fba61a82da898a05c529cec5315e1
firshandy <firshandydwi@gmail.com>
2024-10-22 11:40:30 +0700

    last

---
+++ servicepage/script.js
@@ -0,0 +1,15 @@
+// toogle navbar
+let navToggle = document.querySelector(".nav_toggle");
+let navWrapper = document.querySelector(".nav_wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
